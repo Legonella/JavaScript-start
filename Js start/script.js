852c46215946889118a36f1b65e43003cb37dbc5
Legonella <dastbravis@gmail.com>
2020-10-07 13:05:02 +0200

    Java start

---
+++ Js start/script.js
@@ -0,0 +1,45 @@
+"use strict";
+/* 
+let message;
+message = 'Hello!';  //Значение сохранилось в переменной
+alert(message); */
+
+/* let message = 'Hello!';  // Краткий вариант записи
+alert(message); */
+
+/* let user = 'Andrey';  // Удобная запись для лучшей читаемости кода
+    let age = 18;
+   let message = 'Hello!';
+alert(user);
+alert(age);
+alert(message); */
+
+/* var message = 'Hello!'; // Устаревший вариант присваивания переменной
+alert(message); */
+
+/* let message = 'Andrey'; // Присваивание одной переменной значение другой
+let hello = message;
+alert(hello); */
+
+/* let $ = 5; // объявили переменную с именем "$"
+let _ = 5;    // а теперь переменную с именем "_"
+alert($ + _); // 10 
+*/
+
+/* let 1 a = 5;  // не может начинаться с цифры
+let my-name = 'Andrey'; // дефис '-' не разрешён в имени
+ */
+
+/* const myBirthday = '30.07.2002'; // Константа, значение минять запрещенно!
+myBirthday = '01.01.2001'; */ // Выдаст ошибку
+
+/* const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// Когда нам нужно выбрать значение (цвет)
+
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00 */
+
